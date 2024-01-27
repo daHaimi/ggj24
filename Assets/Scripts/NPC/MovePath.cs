8a5648c84f0ebae7644809de213172bb0331e496
Mathias Haimerl <mathias.haimerl@thi.de>
2024-01-27 18:56:32 +0100

    feat: wait time at waypoints

--- Assets/Scripts/NPC/MovePath.cs
+++ Assets/Scripts/NPC/MovePath.cs
@@ -1,39 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class MovePath : MonoBehaviour
 {
-
     public Transform[] waypoints;
     public LineRenderer line;
+    public float waitTimeAtWaypoint = 0;
+
+    public AnimatorController idleAnimation;
+    public AnimatorController walkAnimation;
     
     private int waypointIdx;
     private Vector3 target;
+    private float _yetToWait = 0;
+    private bool walking = false;
     
     private NavMeshAgent agent;
+    private Animator _animator;
 
 
     private void Start()
     {
+        _animator = GetComponentInChildren<Animator>();
         agent = GetComponent<NavMeshAgent>();
+        target = waypoints[waypointIdx].position;
         UpdateDestination();
     }
 
     private void Update()
     {
-        if (Vector3.Distance(transform.position, target) < 1)
+        // Nicht wartend
+        if (_yetToWait <= 0)
         {
-            IterateIndex();
-            UpdateDestination();
+            // Angekommen
+            if (Vector3.Distance(transform.position, target) < 1)
+            {
+                Debug.Log("Reached Waypoint");
+                _animator.runtimeAnimatorController = idleAnimation;
+                IterateIndex();
+                _yetToWait = waitTimeAtWaypoint;
+                walking = false;
+            }
+            else if (!walking)
+            {
+                Debug.Log("To next Waypoint");
+                walking = true;
+                _animator.runtimeAnimatorController = walkAnimation;
+                UpdateDestination();
+            }
+        } 
+        else
+        {
+            _yetToWait -= Time.deltaTime;
         }
     }
 
     public void UpdateDestination()
     {
-        target = waypoints[waypointIdx].position;
         agent.SetDestination(target);
     }
 
@@ -41,6 +68,7 @@ public class MovePath : MonoBehaviour
     {
         if (waypoints.Length < 1) return; 
         waypointIdx = (waypointIdx + 1) % waypoints.Length;
+        target = waypoints[waypointIdx].position;
     }
     private void OnDrawGizmos()
     {
