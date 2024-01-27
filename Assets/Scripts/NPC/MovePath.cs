23c1c5edffc5e5a320105c4b4531bc840779aa3c
Mathias Haimerl <mathias.haimerl@thi.de>
2024-01-27 18:12:46 +0100

    Added Path following for NPC

--- Assets/Scripts/NPC/MovePath.cs
+++ Assets/Scripts/NPC/MovePath.cs
@@ -1,56 +1,56 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MovePath : MonoBehaviour
 {
-    [HideInInspector] public float _walkPointThreshold = 0.5f;
-    [HideInInspector] public int w;
-    [HideInInspector] public bool forward = true;
-    [HideInInspector] public Vector3 finishPos;
-    [HideInInspector] public int targetPointsTotal;
-    [HideInInspector] public bool loop;
-    public LinkedList<Vector3> points = new();
 
-    private int _nextTargetIndex;
+    public Transform[] waypoints;
+    public LineRenderer line;
+    
+    private int waypointIdx;
+    private Vector3 target;
+    
+    private NavMeshAgent agent;
 
-    public void InitStartPosition(int _w, int _i)
+
+    private void Start()
     {
-        forward = true;
+        agent = GetComponent<NavMeshAgent>();
+        UpdateDestination();
+    }
 
-        w = _w;
-        targetPointsTotal = points.Count - 2;
-
-        if (_i < targetPointsTotal && _i > 0)
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, target) < 1)
         {
-            if (forward)
-            {
-                _nextTargetIndex = _i + 1;
-                //finishPos = points.Find(w, _i + 1);
-            }
-            else
-            {
-                _nextTargetIndex = _i;
-                //finishPos = _WalkPath.getNextPoint(w, _i);
-            }
-        }
-        else
-        {
-            if (forward)
-            {
-                _nextTargetIndex = 1;
-                //finishPos = _WalkPath.getNextPoint(w, 1);
-            }
-            else
-            {
-                _nextTargetIndex = targetPointsTotal;
-               // finishPos = _WalkPath.getNextPoint(w, targetPointsTotal);
-            }
+            IterateIndex();
+            UpdateDestination();
         }
     }
 
-    public void SetLookPosition()
+    public void UpdateDestination()
     {
-        Vector3 targetPos = new Vector3(finishPos.x, transform.position.y, finishPos.z);
-        transform.LookAt(targetPos);
+        target = waypoints[waypointIdx].position;
+        agent.SetDestination(target);
+    }
+
+    public void IterateIndex()
+    {
+        if (waypoints.Length < 1) return; 
+        waypointIdx = (waypointIdx + 1) % waypoints.Length;
+    }
+    private void OnDrawGizmos()
+    {
+        if(line == null || waypoints.Length < 2) //if the path has 1 or no corners, there is no need
+            return;
+
+        line.positionCount = waypoints.Length; //set the array of positions to the amount of corners
+
+        for(var i = 0; i < waypoints.Length; i++){
+            line.SetPosition(i, waypoints[i].position);
+        }
     }
 }
